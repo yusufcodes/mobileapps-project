80a4694abbac1da41f9b135b5c90f1c455501baa
yusufcodes <yusufcodes@outlook.com>
2021-01-26 20:11:54 +0000

    Rendered list of coffee shops based on call to API

--- coffidaApp/components/CoffeeShop/AllShops.js
+++ coffidaApp/components/CoffeeShop/AllShops.js
@@ -1,29 +1,60 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
+import {List} from 'react-native-paper';
+
+import getToken from '../../functions/getToken';
 
 const axios = require('axios');
 
-export default function AllShops() {
+export default function AllShops({navigation}) {
+  // Storing token in state to be referenced later
+  const [token, setToken] = useState('');
+  // Storing details of each shop to output to the user
+  const [shops, setShops] = useState([]);
+
   useEffect(() => {
+    // Assigning token value to state once the Promise from 'value' is resolved
+    const setTokenState = async () => {
+      const value = await getToken();
+      setToken(value);
+    };
+
     async function response() {
       try {
-        //   const response = await axios.get('http://10.0.2.2:3333/api/1.0.0/find', headers: {'X-Authorization': 'b2c4070f584a41e55f5b93437b1857a5'});
         const response = await axios({
           method: 'get',
           url: 'http://10.0.2.2:3333/api/1.0.0/find',
           responseType: 'json',
-          headers: {'X-Authorization': 'b2c4070f584a41e55f5b93437b1857a5'},
+          headers: {'X-Authorization': token},
         });
-        console.log(response.data);
+
+        const arrayOfShops = response.data.map((item, index) => ({
+          id: item.location_id,
+          name: item.location_name,
+          town: item.location_town,
+          rating: item.avg_overall_rating,
+        }));
+        setShops(arrayOfShops);
       } catch (error) {
         console.log(error);
       }
     }
+
+    setTokenState();
     response();
-  });
+  }, []);
+
   return (
     <View>
-      <Text>all Shops</Text>
+      {shops?.map((item, index) => (
+        <List.Item
+          key={index}
+          title={`${item.name}`}
+          description={`Town: ${item.town} \nRating: ${item.rating}`}
+          descriptionNumberOfLines={2}
+          left={(props) => <List.Icon {...props} icon="coffee" />}
+        />
+      ))}
     </View>
   );
 }
