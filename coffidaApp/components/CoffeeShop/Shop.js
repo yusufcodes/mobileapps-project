18e4d2d0ae8ed2cbbf5e31a80ca1b170d3ec24fd
yusufcodes <yusufcodes@outlook.com>
2021-02-01 16:03:19 +0000

    Added section to display list of reviews for current location

--- coffidaApp/components/CoffeeShop/Shop.js
+++ coffidaApp/components/CoffeeShop/Shop.js
@@ -1,8 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, ScrollView} from 'react-native';
-import {ActivityIndicator, Colors} from 'react-native-paper';
+import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
+import {
+  ActivityIndicator,
+  Colors,
+  Paragraph,
+  Divider,
+  Title,
+} from 'react-native-paper';
 import getToken from '../../functions/getToken';
 import Heading from './Shop/Heading';
+import LikeButton from '../Global/LikeButton';
 
 const axios = require('axios');
 
@@ -43,6 +50,8 @@ export default function Shop({route}) {
           reviews,
         });
         console.log(`Retrieved single shop`);
+        console.log('Reviews:');
+        console.log(reviews);
       } catch (error) {
         console.log(error);
       }
@@ -51,15 +60,70 @@ export default function Shop({route}) {
     response();
   }, []);
 
+  const handleLike = () => {
+    console.log('Like button pressed');
+  };
+
+  const styles = StyleSheet.create({
+    root: {},
+    like: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  });
+
   let renderShop = (
     <ActivityIndicator animating size="large" color={Colors.red800} />
   );
 
   if (shop) {
     renderShop = (
-      <View>
+      <ScrollView style={styles.root}>
         <Heading details={shop} />
-      </View>
+        <Title
+          style={{
+            marginLeft: 20,
+            marginRight: 20,
+          }}>
+          Reviews
+        </Title>
+        {shop.reviews.map(
+          ({
+            review_body,
+            overall_rating,
+            price_rating,
+            quality_rating,
+            clenliness_rating,
+            likes,
+          }) => (
+            <>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginLeft: 20,
+                  marginRight: 20,
+                }}>
+                <Paragraph
+                  style={{
+                    width: '70%',
+                  }}>
+                  {review_body}
+                </Paragraph>
+                <View>
+                  <Paragraph>
+                    {`Overall: ${overall_rating}/5 \nPrice: ${price_rating}/5 \nQuality: ${quality_rating}/5  \nCleanliness: ${clenliness_rating}/5`}
+                  </Paragraph>
+                  <View style={styles.like}>
+                    <LikeButton handler={handleLike} size={20} />
+                    <Paragraph>{likes}</Paragraph>
+                  </View>
+                </View>
+              </View>
+              <Divider />
+            </>
+          ),
+        )}
+      </ScrollView>
     );
   }
   return <View>{renderShop}</View>;
