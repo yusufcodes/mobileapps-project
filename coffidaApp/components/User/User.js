83fb7043372ddf66044b7e63f22ecd26357bfc7c
yusufcodes <yusufcodes@outlook.com>
2021-02-03 10:38:48 +0000

    Built user view

--- coffidaApp/components/User/User.js
+++ coffidaApp/components/User/User.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Title, Subheading, Paragraph} from 'react-native-paper';
+import Button from '../Global/Button';
 
-export default function User() {
+const styles = StyleSheet.create({
+  root: {
+    padding: 30,
+  },
+  heading: {
+    textAlign: 'center',
+  },
+  singleDetail: {
+    flexDirection: 'row',
+  },
+});
+
+export default function User({navigation}) {
+  const displayUpdate = () => {
+    navigation.navigate('Update');
+  };
+
   return (
-    <View>
-      <Text>User Settings Screen</Text>
+    <View style={styles.root}>
+      <Title style={styles.heading}>My Profile</Title>
+      <View style={styles.details}>
+        <Subheading>Personal Details</Subheading>
+        <View style={styles.singleDetail}>
+          <Paragraph style={{fontWeight: 'bold'}}>First Name: </Paragraph>
+          <Paragraph>Yusuf</Paragraph>
+        </View>
+        <View style={styles.singleDetail}>
+          <Paragraph style={{fontWeight: 'bold'}}>Last Name: </Paragraph>
+          <Paragraph>Chowdhury</Paragraph>
+        </View>
+        <View style={styles.singleDetail}>
+          <Paragraph style={{fontWeight: 'bold'}}>Email: </Paragraph>
+          <Paragraph>17031422@stu.mmu.ac.uk</Paragraph>
+        </View>
+      </View>
+      <Button text="Update Details" handler={displayUpdate} />
     </View>
   );
 }
