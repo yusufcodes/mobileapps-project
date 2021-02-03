d92d5ff10f212d6c10a35e58e20109a5ab204096
yusufcodes <yusufcodes@outlook.com>
2021-02-03 10:38:31 +0000

    Built update view

--- coffidaApp/components/User/Update.js
+++ coffidaApp/components/User/Update.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Title, Subheading, Paragraph, TextInput} from 'react-native-paper';
+import Button from '../Global/Button';
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 30,
+  },
+  heading: {
+    textAlign: 'center',
+  },
+});
 
 export default function Update() {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const updateHandler = () => {
+    console.log('To do: perform request to update user details');
+  };
   return (
-    <View>
-      <Text>Update details</Text>
+    <View style={styles.root}>
+      <Title style={styles.heading}>Update Details</Title>
+      <TextInput
+        label="firstName"
+        value={firstName}
+        mode="outlined"
+        onChangeText={(firstName) => setFirstName(firstName)}
+      />
+      <TextInput
+        label="lastName"
+        value={lastName}
+        mode="outlined"
+        onChangeText={(lastName) => setLastName(lastName)}
+      />
+      <TextInput
+        label="email"
+        value={email}
+        mode="outlined"
+        onChangeText={(email) => setEmail(email)}
+      />
+      <Button text="Confirm Details" handler={updateHandler} />
     </View>
   );
 }
