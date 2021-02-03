fbe359b9d7afe8b040f1d8fb7b194de4b7babbd1
yusufcodes <yusufcodes@outlook.com>
2021-02-03 11:04:15 +0000

    Added network request to retrieve user details

--- coffidaApp/components/User/User.js
+++ coffidaApp/components/User/User.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Title, Subheading, Paragraph} from 'react-native-paper';
 import Button from '../Global/Button';
+import getToken from '../../functions/getToken';
+import getId from '../../functions/getId';
+
+const axios = require('axios');
 
 const styles = StyleSheet.create({
   root: {
@@ -16,9 +20,42 @@ const styles = StyleSheet.create({
 });
 
 export default function User({navigation}) {
-  const displayUpdate = () => {
-    navigation.navigate('Update');
-  };
+  const [details, setDetails] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [likedReviews, setLikedReviews] = useState([]);
+
+  const displayUpdate = () => navigation.navigate('Update');
+  useEffect(() => {
+    async function response() {
+      const token = await getToken();
+      const id = await getId();
+      try {
+        const response = await axios({
+          method: 'get',
+          url: `http://10.0.2.2:3333/api/1.0.0/user/${parseInt(id)}`,
+          responseType: 'json',
+          headers: {'X-Authorization': token},
+        });
+        const {
+          email,
+          first_name,
+          last_name,
+          favourite_locations,
+          liked_reviews,
+          reviews,
+        } = response.data;
+        setDetails([email, first_name, last_name]);
+        setLocations(favourite_locations);
+        setReviews(reviews);
+        setLikedReviews(liked_reviews);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    response();
+  }, []);
 
   return (
     <View style={styles.root}>
