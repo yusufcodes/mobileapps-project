86cc6ddfc5f1a4c196f980d8c1007069ee409b2b
yusufcodes <yusufcodes@outlook.com>
2021-02-25 23:28:52 +0000

    Cleanup

--- coffidaApp/components/Navigation/UserStackScreen.js
+++ coffidaApp/components/Navigation/UserStackScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {IconButton, Dialog, Portal, Button} from 'react-native-paper';
 import {createStackNavigator} from '@react-navigation/stack';
 import User from '../User/User';
 import Update from '../User/Update';
@@ -7,16 +6,6 @@ import EditReview from '../CoffeeShop/EditReview';
 
 export default function UserStackScreen() {
   const UserStackScreen = createStackNavigator();
-
-  const handleLogout = () => {
-    /*
-    1. Show dialog: "Are you sure you want to log out?"
-    2. Options: Cancel Yes
-    3. Press Yes -> perform logout
-    4. Press Cancel -> Hide Dialog
-    
-    */
-  };
 
   return (
     <UserStackScreen.Navigator>
